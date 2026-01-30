3200de060b5b051b20a7eaacfe00d6f4655e28ad
Muhammad Zain <147588685+ZainSharafat@users.noreply.github.com>
2026-01-30 23:35:58 +0000

    all about access elements

---
+++ DOM/dom.js
@@ -0,0 +1,11 @@
+// const heading = document.getElementById("heading"); //h1
+// console.dir(heading);
+
+// const multiHeading = document.getElementsByClassName("heading")
+// console.dir(multiHeading);
+
+// const paras = document.getElementsByTagName("p")
+// console.dir(paras)
+
+
+//query selector
